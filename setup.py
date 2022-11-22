6d8f5c3c1f6d829324918163680ad8a249785245
sharad28 <saradmishra28@gmail.com>
2022-11-23 00:10:02 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "MLflow-project-template-Sarad"
+REPO_NAME = "Super Resolution GAN"
 AUTHOR_USER_NAME = "sharad28"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
